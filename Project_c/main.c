9cca44df3017d7629edbac7202034fbbdf78c945
Seyyid Ahmed Doğan <seyyidahmed.dogann@gmail.com>
2015-12-16 23:09:51 +0200

    .

--- Project_c/main.c
+++ Project_c/main.c
@@ -1,5 +1,5 @@
 ///reference http://en.cppreference.com/w/c/io/setbuf
-///thanks to guesst on http://cboard.cprogramming.com/csharp-programming/101684-calculate-elapsed-seconds.html
+///thanks to Stephen on http://stackoverflow.com/questions/3557221/how-do-i-measure-time-in-c
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,23 +8,16 @@
 
 #define BUFFERSIZE 3
 
-void testPutcBuffered();
+void testPutcBuffered(int);
+void testFputcBuffered(int);
 
 
 int main()
 {
 
-//    char buffer[BUFFERSIZE];
-//    int c;
-//    c = setvbuf(stdin, buffer, _IOFBF, BUFFERSIZE);
-//    for (c = 0;c<100000;c++)
-//    {
-//        putchar('0');
-//    }
 
-
-    testPutcBuffered(10000000);
-
+    testPutcBuffered(1000000);
+    testFputcBuffered(1000000);
 
 
 
@@ -78,3 +71,55 @@ void testPutcBuffered(int times)
     printf ("Putc() Test Buffered took %.2lf seconds to run for %d times.\n", dif,times);
 
 }
+
+
+void testFputcBuffered(int times)
+{
+    time_t start, end;
+    double dif;
+
+    //get start time
+    time (&start);
+
+    //open the test file.
+    FILE *fp;
+    fp = fopen("Test.txt","w");
+    if(fp == NULL)
+    {
+        printf("Test.txt couldnt open \non testFputcBuffered()");
+        exit(1);
+    }
+
+    //allocate for buffer and assign it
+    char buffer[BUFFERSIZE];
+    if(setvbuf(fp, buffer, _IOFBF, BUFFERSIZE) != 0)
+    {
+        printf("setvbuf() error");
+        exit(2);
+    }
+
+    //plainly put "times" times random character
+    int c,i;
+    for(i = 0;i<times;i++)
+    {
+        c = (rand()%26)+'A';
+        fputc(c,fp);
+    }
+
+    //dont forget to close the file after you are done with it.
+    fclose(fp);
+
+    //get stop time
+    time (&end);
+
+    //calculate the seconds between start and end times.
+    dif = difftime (end,start);
+
+    //print the result!?
+    printf ("Fputc() Test Buffered took %.2lf seconds to run for %d times.\n", dif,times);
+}
+
+
+
+
+
