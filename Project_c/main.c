37d7cdf55fdf06ae0255f8cba81c82865538eb30
Seyyid Ahmed Doğan <seyyidahmed.dogann@gmail.com>
2015-12-16 22:44:09 +0200

    testputcbuffed added

--- Project_c/main.c
+++ Project_c/main.c
@@ -1,25 +1,72 @@
 ///reference http://en.cppreference.com/w/c/io/setbuf
+///thanks to guesst on http://cboard.cprogramming.com/csharp-programming/101684-calculate-elapsed-seconds.html
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h> //for INT_MAX
+#include <time.h> //for timer
 
 #define BUFFERSIZE 3
+
+void testPutcBuffered();
+
 
 int main()
 {
 
-    char buffer[BUFFERSIZE];
-    int c;
-    //c = setvbuf(stdin, buffer, _IOFBF, BUFFERSIZE);
-    for (c = 0;c<30000;c++)
+//    char buffer[BUFFERSIZE];
+//    int c;
+//    c = setvbuf(stdin, buffer, _IOFBF, BUFFERSIZE);
+//    for (c = 0;c<100000;c++)
+//    {
+//        putchar('0');
+//    }
+
+
+    testPutcBuffered(10000000);
+
+
+
+
+    return 0;
+}
+
+
+void testPutcBuffered(int times)
+{
+    time_t start, end;
+    double dif;
+
+    time (&start);
+
+
+    FILE *fp;
+    fp = fopen("Test.txt","w");
+    if(fp == NULL)
     {
-        putchar('0');
+        printf("Test.txt couldnt open \non testPutcBuffered()");
+        exit(1);
     }
 
+    char buffer[BUFFERSIZE];
+    if(setvbuf(fp, buffer, _IOFBF, BUFFERSIZE) != 0)
+    {
+        printf("setvbuf() error");
+        exit(2);
+    }
 
+    int c,i;
 
+    for(i = 0;i<times;i++)
+    {
+        c = (rand()%26)+'A';
+        putc(c,fp);
+    }
 
-    printf("Hello world!\n");
-    return 0;
+    fclose(fp);
+
+    time (&end);
+    dif = difftime (end,start);
+    printf ("Putc() Test Buffered took %.2lf seconds to run for %d times.\n", dif,times);
+
 }
